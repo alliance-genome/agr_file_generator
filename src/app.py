aef1459ecdbe1f0527a115a0002cab6df30f3b8e
Adam <adam.j.wright82@gmail.com>
2019-05-28 12:26:50 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ else:
 
 uri = "bolt://" + host + ":" + str(port)
 
-if "ALLIANCE_DB_VERSION" in os.environ:
+if "ALLIANCE_DATABASE_VERSION" in os.environ:
     alliance_db_version = int(os.environ['ALLIANCE_DATABASE_VERSION'])
 else:
     alliance_db_version = "test"
