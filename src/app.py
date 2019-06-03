f643bcc705403aa0cbebd94c2ad5a8ac8d8c9ad0
Matt Russell <matthew.russell@wormbase.org>
2019-06-03 16:38:02 +0100

    Minor style improvements.

--- src/app.py
+++ src/app.py
@@ -1,22 +1,15 @@
-from vcf_file_generator import VcfFileGenerator
 import os
 
-if "NEO4J_HOST" in os.environ:
-    host = os.environ['NEO4J_HOST']
-else:
-    host = "localhost"
+from vcf_file_generator import VcfFileGenerator
 
-if "NEO4J_PORT" in os.environ:
-    port = int(os.environ['NEO4J_PORT'])
-else:
-    port = 7687
+
+host = os.environ.get('NEO4J_HOST', 'localhost')
+
+port = int(os.environ.get('NEO4J_PORT', 7687))
+
+alliance_db_version = os.environ.get('ALLIANCE_DATABASE_VERSION', 'test')
 
 uri = "bolt://" + host + ":" + str(port)
-
-if "ALLIANCE_DATABASE_VERSION" in os.environ:
-    alliance_db_version = os.environ['ALLIANCE_DATABASE_VERSION']
-else:
-    alliance_db_version = "test"
 
 if __name__ == '__main__':
     generated_files_folder = "generated_files"
