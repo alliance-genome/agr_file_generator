e4b87a998580ec08b1b411c84098be5421fe5e96
Matt Russell <matthew.russell@wormbase.org>
2019-06-04 15:16:32 +0100

    Housekeeping.
    - Removed unused imports
    - Use single-quote string literals throughout.
    - Indentation.
    - Whitespace.

--- src/assemblySequence.py
+++ src/assemblySequence.py
@@ -1,38 +1,35 @@
-import sys, os
+import os
+
 import wget
-import gzip
-import shutil
 from pyfaidx import Fasta
 
-class AssemblySequence(object):
-    def __init__(self, assembly):
+class AssemblySequence:
+
+    def __init__(self, assembly, local_assembly_dir='sequences'):
         self.assembly = assembly
-
-        local_assembly_dir = "sequences/"
-        local_filepath = local_assembly_dir +  assembly + ".fa"
-
+        local_filepath = os.path.join(local_assembly_dir, assembly + '.fa')
         if not os.path.isfile(local_filepath):
-            print(local_filepath)
-            print("file does not exists")
-            AssemblySequence.__downloadAssembly(assembly, local_assembly_dir, local_filepath)
-
+            print('Assembly file does not exist:')
+            self.download(assembly, local_assembly_dir, local_filepath)
         self.fa = Fasta(local_filepath)
 
-    def __downloadAssembly(assembly, local_assembly_dir, local_filepath):
+    @classmethod
+    def download(cls, assembly, local_assembly_dir, local_filepath):
         assembly_to_s3_dict = {
-                "R6.27": "https://s3.amazonaws.com/agrjbrowse/FlyBase/fruitfly/fasta/",
-                "GRCz11": "https://s3.amazonaws.com/agrjbrowse/zfin/zebrafish/fasta/",
-                "WBcel235": "https://s3.amazonaws.com/agrjbrowse/WormBase/c_elegans_PRJNA13758/fasta/",
-                "Rnor_6.0": "https://s3.amazonaws.com/agrjbrowse/RGD/rat/fasta/",
-                "GRCm38": "https://s3.amazonaws.com/agrjbrowse/MGI/mouse/fasta/"}
-        url = assembly_to_s3_dict[assembly] + assembly + ".fa"
+            'R6.27': 'https://s3.amazonaws.com/agrjbrowse/FlyBase/fruitfly/fasta/',
+            'GRCz11': 'https://s3.amazonaws.com/agrjbrowse/zfin/zebrafish/fasta/',
+            'WBcel235': 'https://s3.amazonaws.com/agrjbrowse/WormBase/c_elegans_PRJNA13758/fasta/',
+            'Rnor_6.0': 'https://s3.amazonaws.com/agrjbrowse/RGD/rat/fasta/',
+            'GRCm38': 'https://s3.amazonaws.com/agrjbrowse/MGI/mouse/fasta/'
+        }
+        url = assembly_to_s3_dict[assembly] + assembly + '.fa'
         if not os.path.exists(local_assembly_dir):
             os.makedirs(local_assembly_dir)
         wget.download(url, out=local_assembly_dir)
 
     def get(self, chromosome, start, end):
-        start = start - 1 
-        if chromosome.startswith("chr"):
+        start -= 1
+        if chromosome.startswith('chr'):
             chromosome_str = chromosome[3:]
         else:
             chromosome_str = chromosome
