3f0c154ecc11fdf278d3c71174c8ae51c073465b
Adam Wright <adam.j.wright82@gmail.com>
2019-06-28 15:35:58 -0400

    made it so that the pared down daf is being generated. Next fill in all columns

--- src/agr/app.py
+++ src/agr/app.py
@@ -3,6 +3,7 @@ import os
 
 from agr.vcf_file_generator import VcfFileGenerator
 from agr.orthology_file_generator import OrthologyFileGenerator
+from agr.daf_file_generator import DafFileGenerator
 import agr.assembly_sequence as agr_asm_seq
 from agr.data_source import DataSource
 
@@ -14,7 +15,6 @@ port = int(os.environ.get('NEO4J_PORT', 7687))
 alliance_db_version = os.environ.get('ALLIANCE_DATABASE_VERSION', 'test')
 
 uri = "bolt://" + host + ":" + str(port)
-
 assembly_to_s3_uri = {
     'R6.27': 'https://s3.amazonaws.com/agrjbrowse/FlyBase/fruitfly/fasta/',
     'GRCz11': 'https://s3.amazonaws.com/agrjbrowse/zfin/zebrafish/fasta/',
@@ -31,8 +31,9 @@ def setup_logging(logger_name):
 def main(generated_files_folder='generated_files',
          fasta_sequences_folder='sequences',
          skip_chromosomes={'Unmapped_Scaffold_8_D1580_D1567'}):
-    #  generate_vcf_files(generated_files_folder, fasta_sequences_folder, skip_chromosomes)
-    generate_orthology_file(generated_files_folder, alliance_db_version)
+    #generate_vcf_files(generated_files_folder, fasta_sequences_folder, skip_chromosomes)
+    #generate_orthology_file(generated_files_folder, alliance_db_version)
+    generate_daf_file(generated_files_folder, alliance_db_version)
     exit()
 
 
@@ -64,6 +65,7 @@ RETURN c.primaryKey AS chromosome,
                            alliance_db_version)
     gvf.generate_files(skip_chromosomes=skip_chromosomes)
 
+
 def generate_orthology_file(generated_files_folder, alliance_db_version):
     orthology_query = """MATCH (species1)<-[sa:FROM_SPECIES]-(gene1:Gene)-[o:ORTHOLOGOUS]->(gene2:Gene)-[sa2:FROM_SPECIES]->(species2:Species)
 WHERE o.strictFilter
@@ -89,6 +91,36 @@ RETURN gene1.primaryKey AS gene1ID,
                                 generated_files_folder,
                                 alliance_db_version)
     of.generate_file()
+
+
+def generate_daf_file(generated_files_folder, alliance_db_version):
+    daf_query = """MATCH (dej:DiseaseEntityJoin)-[]-(object)-[da]->(disease:DOTerm)
+WHERE da.uuid = dej.primaryKey
+MATCH (object)-[FROM_SPECIES]->(species:Species)
+OPTIONAL MATCH (ec:EvidenceCode)-[EVIDENCE]-(dej)
+OPTIONAL MATCH (p:Publication)-[ev:EVIDENCE]-(dej)
+OPTIONAL MATCH (object)-[o:ORTHOLOGOUS]-(oGene:Gene)
+WHERE o.strictFilter AND (ec.primaryKey = "ISS" OR ec.primaryKey = "ISO")
+OPTIONAL MATCH (object)-[IS_ALLELE_OF]->(gene:Gene)
+RETURN  object.taxonId AS taxonId,
+        species.name AS speciesName,
+        collect(oGene.primaryKey) AS withOrthologs,
+        labels(object) AS objectType,
+        object.primaryKey AS dbObjectID,
+        object.symbol AS dbObjectSymbol,
+        p.pubMedId AS pubMedID,
+        p.pubModId As pubModID,
+        type(da) AS associationType,
+        collect(DISTINCT gene.primaryKey) AS inferredGeneAssociation,
+        disease.doId AS DOID,
+        disease.name as DOname,
+        ec.primaryKey AS evidenceCode,
+        da.dataProvider AS dataProvider"""
+    data_source = DataSource(uri, daf_query)
+    daf = DafFileGenerator(data_source,
+                           generated_files_folder,
+                           alliance_db_version)
+    daf.generate_file() 
 
 
 if __name__ == '__main__':
--- src/agr/daf_file_generator.py
+++ src/agr/daf_file_generator.py
@@ -5,23 +5,13 @@ from dateutil.parser import parse
 from datetime import datetime
 from time import gmtime, strftime
 
-from neo4j.v1 import GraphDatabase
-
-uri = "staging.alliancegenome.org:7687"
-driver = GraphDatabase.driver(uri)
-
-outputFile = sys.argv[1]
-databaseVersion = sys.argv[2]
 
 logger = logging.getLogger(name=__name__)
 
+
 class DafFileGenerator:
 
-    disease_file = open(outputFile,'w')
-
-    datetimeNow = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-
-    header = """#########################################################################
+    file_header_template = """#########################################################################
 #
 # Disease Association Format (DAF)
 # Source: Alliance of Genome Resources (Alliance)
@@ -30,63 +20,70 @@ class DafFileGenerator:
 # Date: {datetimeNow}
 #
 #########################################################################
-""".format(datetimeNow = datetimeNow, databaseVersion=databaseVersion)
+"""
 
+    def __init__(self, disease_associations, generated_files_folder, database_version):
+        self.disease_associations = disease_associations
+        self.database_version = database_version
+        self.generated_files_folder = generated_files_folder
 
-    def createDaf:
-        disease_file.write(header)
+    @classmethod
+    def _generate_header(cls, database_version):
+        return cls.file_header_template.format(datetimeNow=strftime("%Y-%m-%d %H:%M:%S", gmtime()),
+                databaseVersion=database_version)
+
+    def generate_file(self):
+        outputFilepath = self.generated_files_folder + "/agr-daf-" + self.database_version + ".tsv"
+        disease_file = open(outputFilepath,'w')
+        disease_file.write(self._generate_header(self.database_version))
     
-        disease_file.write("Taxon\tTaxonName\tDBobjectType\tDBObjectID\tDBObjectSymbol\tInferredGeneAssociation\tAssociationType\tDOID\tDOname\twithOrthologs\tEvidenceCode\tReference\tAssignedBy\n")
-        with session.begin_transaction() as tx:
-            for record in tx.run("""MATCH (dej:DiseaseEntityJoin)-[]-(object)-[da]->(disease:DOTerm)
-                                    WHERE da.uuid = dej.primaryKey
-                                    MATCH (object)-[FROM_SPECIES]->(species:Species)
-                                    OPTIONAL MATCH (ec:EvidenceCode)-[EVIDENCE]-(dej)
-                                    OPTIONAL MATCH (p:Publication)-[ev:EVIDENCE]-(dej)
-                                    OPTIONAL MATCH (object)-[o:ORTHOLOGOUS]-(oGene:Gene)
-                                    WHERE o.strictFilter AND (ec.primaryKey = "ISS" OR ec.primaryKey = "ISO")
-                                    OPTIONAL MATCH (object)-[IS_ALLELE_OF]->(gene:Gene)
-                                    RETURN  object.taxonId AS taxonId,
-                                            species.name AS speciesName,
-                                            collect(oGene.primaryKey) AS withOrthologs,
-                                            labels(object) AS objectType,
-                                            object.primaryKey AS dbObjectID,
-                                            object.symbol AS dbObjectSymbol,
-                                            p.pubMedId AS pubMedID,
-                                            p.pubModId As pubModID,
-                                            type(da) AS associationType,
-                                            collect(DISTINCT gene.primaryKey) AS inferredGeneAssociation,
-                                            disease.doId AS DOID,
-                                            disease.name as DOname,
-                                            ec.primaryKey AS evidenceCode,
-                                            da.dataProvider AS dataProvider
-                                            """):
+        columns = ["Taxon",
+                   "TaxonName",
+                   "DBobjectType",
+                   "DBObjectID",
+                   "DBObjectSymbol",
+                   "InferredGeneAssociation",
+                   "AssociationType",
+                   "DOID",
+                   "DOname",
+                   "withOrthologs",
+                   "EvidenceCode",
+                   "Reference",
+                   "AssignedBy"]
+        disease_file.write("\t".join(columns) + "\n")
+        for disease_association in self.disease_associations:
+            dbObjectType = "allele" if disease_association["objectType"][0] == "Feature" else disease_association["objectType"][0].lower()
+            pubID = disease_association["pubMedID"] if disease_association["pubMedID"] else disease_association["pubModID"]
+            if pubID is None:
+                pubID = ""
+ 
+            withOrthologs = "|".join(set(disease_association["withOrthologs"])) if disease_association["withOrthologs"] else ""
+            DOname = disease_association["DOname"] if disease_association["DOname"] else ""
+            inferredGeneAssociation = ""
+            if dbObjectType == "gene":
+                inferredGeneAssociation = disease_association["dbObjectID"]
+            elif dbObjectType == "allele":
+                inferredGeneAssociation = ",".join(disease_association["inferredGeneAssociation"])
+
+            if disease_association["evidenceCode"] is not None:
+                evidenceCode = disease_association["evidenceCode"]
+            else:
+                evidenceCode = ""
+
+            disease_file.write("\t".join([disease_association["taxonId"],
+                                          disease_association["speciesName"],
+                                          dbObjectType,
+                                          disease_association["dbObjectID"],
+                                          disease_association["dbObjectSymbol"],
+                                          inferredGeneAssociation,
+                                          disease_association["associationType"].lower(),
+                                          disease_association["DOID"],
+                                          DOname,
+                                          withOrthologs,
+                                          evidenceCode,
+                                          pubID,
+                                          disease_association["dataProvider"]])
+                             + "\n")
     
-                dbObjectType = "allele" if record["objectType"][0] == "Feature" else record["objectType"][0].lower()
-                pubID = record["pubMedID"] if record["pubMedID"] else record["pubModID"]
-     
-                withOrthologs = "|".join(set(record["withOrthologs"])) if record["withOrthologs"] else ""
-                DOname = record["DOname"] if record["DOname"] else ""
-                inferredGeneAssociation = ""
-                if dbObjectType == "gene":
-                    inferredGeneAssociation = record["dbObjectID"]
-                elif dbObjectType == "allele":
-                    inferredGeneAssociation = ",".join(record["inferredGeneAssociation"])
-    
-                disease_file.write("\t".join([record["taxonId"],
-                                              record["speciesName"],
-                                              dbObjectType,
-                                              record["dbObjectID"],
-                                              record["dbObjectSymbol"],
-                                              inferredGeneAssociation,
-                                              record["associationType"].lower(),
-                                              record["DOID"],
-                                              DOname,
-                                              withOrthologs,
-                                              record["evidenceCode"],
-                                              pubID,
-    #                                         parse(record["dateProduced"]).strftime("%Y%m%d"),
-                                              record["dataProvider"]])
-                                 + "\n")
-    
+        #parse(disease_association["dateProduced"]).strftime("%Y%m%d"),
         disease_file.close()
