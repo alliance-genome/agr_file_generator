5cc0139dfed02149ffcc52a6ecec7f73d797fffa
Adam Wright <adam.j.wright82@gmail.com>
2019-08-23 09:42:06 -0400

    changing to single quotes

--- src/agr/expression_file_generator.py
+++ src/agr/expression_file_generator.py
@@ -31,65 +31,65 @@ class ExpressionFileGenerator:
                 databaseVersion=database_version)
 
     def generate_file(self):
-        output_filepath = self.generated_files_folder + "/agr-expression-" + self.database_version + ".tsv"
+        output_filepath = self.generated_files_folder + '/agr-expression-' + self.database_version + '.tsv'
         expression_file = open(output_filepath,'w')
         expression_file.write(self._generate_header(self.database_version))
     
-        columns = ["Species",
-                   "SpeciesId",
-                   "GeneID",
-                   "GeneSymbol",
-                   "Location",
-                   "StageID",
-                   "StageTerm",
-                   "AssayID",
-                   "AssayTermName",
-                   "CellularComponentID",
-                   "CellularComponentTerm",
-                   "CellularComponentQualifierIDs",
-                   "CellularComponentQualifierTermNames",
-                   "CellTypeId",
-                   "CellTypeName",
-                   "CellTypeQualifierIDs",
-                   "CellTypeQualifierTermNames",
-                   "AnatomyTermID",
-                   "AnatomyTermName",
-                   "AnatomyTermQaulifierIDs",
-                   "AnatomyTermQualifierTermNames",
-                   "Source",
-                   "Reference"]
+        columns = ['Species',
+                   'SpeciesId',
+                   'GeneID',
+                   'GeneSymbol',
+                   'Location',
+                   'StageID',
+                   'StageTerm',
+                   'AssayID',
+                   'AssayTermName',
+                   'CellularComponentID',
+                   'CellularComponentTerm',
+                   'CellularComponentQualifierIDs',
+                   'CellularComponentQualifierTermNames',
+                   'CellTypeId',
+                   'CellTypeName',
+                   'CellTypeQualifierIDs',
+                   'CellTypeQualifierTermNames',
+                   'AnatomyTermID',
+                   'AnatomyTermName',
+                   'AnatomyTermQaulifierIDs',
+                   'AnatomyTermQualifierTermNames',
+                   'Source',
+                   'Reference']
 
-        expression_file.write("\t".join(columns) + "\n")
+        expression_file.write('\t'.join(columns) + '\n')
 
         for expression in self.expressions:
             print(expression)
-            anatomy_term_id = expression["anatomyTermObj"]["id"] or ""
-            anatomy_term_name = expression["anatomyTermObj"]["term"] or ""
-            anatomy_term_qualifier_ids = ",".join(str(anatomyTermQualifier["primaryKey"]) for anatomyTermQualifier in expression["anatomyTermQualifiers"]) or ""
-            anatomy_term_qualifier_term_names = ",".join(str(anatomyTermQualifier["name"]) for anatomyTermQualifier in expression["anatomyTermQualifiers"]) or ""
+            anatomy_term_id = expression['anatomyTermObj']['id'] or ''
+            anatomy_term_name = expression['anatomyTermObj']['term'] or ''
+            anatomy_term_qualifier_ids = ','.join(str(anatomyTermQualifier['primaryKey']) for anatomyTermQualifier in expression['anatomyTermQualifiers']) or ''
+            anatomy_term_qualifier_term_names = ','.join(str(anatomyTermQualifier['name']) for anatomyTermQualifier in expression['anatomyTermQualifiers']) or ''
 
-            cellular_component_id = expression["cellularComponentObj"]["id"] or ""
-            cellular_component_term = expression["cellularComponentObj"]["term"] or ""
-            cellular_component_qualifier_ids = ",".join(str(cellularComponentQualifierObj["primaryKey"]) for cellularComponentQualifierObj in expression["cellularComponentQualifiers"]) or ""
-            cellular_component_qualifier_term_names = ",".join(str(cellularComponentQualifierObj["name"]) for cellularComponentQualifierObj in expression["cellularComponentQualifiers"]) or ""
+            cellular_component_id = expression['cellularComponentObj']['id'] or ''
+            cellular_component_term = expression['cellularComponentObj']['term'] or ''
+            cellular_component_qualifier_ids = ','.join(str(cellularComponentQualifierObj['primaryKey']) for cellularComponentQualifierObj in expression['cellularComponentQualifiers']) or ''
+            cellular_component_qualifier_term_names = ','.join(str(cellularComponentQualifierObj['name']) for cellularComponentQualifierObj in expression['cellularComponentQualifiers']) or ''
 
-            cell_type_id = expression["cellTypeObj"]["id"] or ""
-            cell_type_term = expression["cellTypeObj"]["term"] or ""
-            cell_type_qualifier_ids = ",".join(str(cellTypeQualifierObj["primaryKey"]) for cellTypeQualifierObj in expression["cellTypeQualifiers"]) or ""
-            cell_type_qualifier_term_names = ",".join(str(cellTypeQualifierObj["name"]) for cellTypeQualifierObj in expression["cellTypeQualifiers"]) or ""
+            cell_type_id = expression['cellTypeObj']['id'] or ''
+            cell_type_term = expression['cellTypeObj']['term'] or ''
+            cell_type_qualifier_ids = ','.join(str(cellTypeQualifierObj['primaryKey']) for cellTypeQualifierObj in expression['cellTypeQualifiers']) or ''
+            cell_type_qualifier_term_names = ','.join(str(cellTypeQualifierObj['name']) for cellTypeQualifierObj in expression['cellTypeQualifiers']) or ''
 
-            stage_id = expression["stageObj"]["id"] or ""
-            stage_term = expression["stageObj"]["term"] or ""
-            assay_id = expression["assayObj"]["id"] or ""
-            assay_term = expression["assayObj"]["term"] or ""
+            stage_id = expression['stageObj']['id'] or ''
+            stage_term = expression['stageObj']['term'] or ''
+            assay_id = expression['assayObj']['id'] or ''
+            assay_term = expression['assayObj']['term'] or ''
 
-            references = ",".join(ref_obj["pubMedId"] or ref_obj["pubModId"] for ref_obj in expression["References"]) or ""
+            references = ','.join(ref_obj['pubMedId'] or ref_obj['pubModId'] for ref_obj in expression['References']) or ''
 
-            expression_file.write("\t".join([expression["speciesObj"]["name"],
-                                             expression["speciesObj"]["id"],
-                                             expression["geneObj"]["id"],
-                                             expression["geneObj"]["symbol"],
-                                             "\"" + expression["location"].replace("'", "\\'").replace(",", "\\,") + "\"",
+            expression_file.write('\t'.join([expression['speciesObj']['name'],
+                                             expression['speciesObj']['id'],
+                                             expression['geneObj']['id'],
+                                             expression['geneObj']['symbol'],
+                                             '\"' + expression['location'].replace(''', '\\'').replace(',', '\\,') + '\"',
                                              stage_id,
                                              stage_term,
                                              assay_id,
@@ -106,8 +106,8 @@ class ExpressionFileGenerator:
                                              anatomy_term_name,
                                              anatomy_term_qualifier_ids,
                                              anatomy_term_qualifier_term_names,
-                                             expression["Source"],
+                                             expression['Source'],
                                              references])
-                             + "\n")
+                             + '\n')
     
         expression_file.close()
