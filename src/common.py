2ad8b34e15bbb9e41243121a131e8ab07df70e49
Adam Wright <adam.j.wright82@gmail.com>
2019-10-15 16:45:29 -0400

    adding common

---
+++ src/common.py
@@ -0,0 +1,27 @@
+import os
+
+import yaml
+import logging
+
+logger = logging.getLogger(__name__)
+
+# Common configuration variables used throughout the script.
+class ContextInfo(object):
+
+    def __init__(self):
+        if os.path.exists('config.yaml'):
+            config_file = open('config.yaml', 'r')
+        else:
+            config_file = open('src/config.yaml', 'r')
+
+        self.config = yaml.load(config_file, Loader=yaml.FullLoader)
+
+        # Look for ENV variables to replace default variables from config file.
+        for key in self.config.keys():
+            try:
+                self.config[key] = os.environ[key]
+            except KeyError:
+                logger.info('Environmental variable not found for \'{}\'. Using config.yaml value.'.format(key))
+                pass  # If we don't find an ENV variable, keep the value from the config file.
+
+        logger.debug('Initialized with config values: {}'.format(self.config))
