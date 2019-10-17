a9e5dcd09f406adeb3be2c6a1cd12a5d3b24ae78
Adam <adam.j.wright82@gmail.com>
2019-10-17 18:22:14 -0400

    changing position for insertion when padded base is added

--- src/generators/vcf_file_generator.py
+++ src/generators/vcf_file_generator.py
@@ -152,6 +152,7 @@ class VcfFileGenerator:
             if variant['genomicVariantSequence'] == '':
                 self._add_padded_base_to_variant(variant, 'deletion')
         elif so_term == 'insertion':
+            variant['POS'] = variant['start']
             if variant['genomicReferenceSequence'] != '':
                 logger.warn('Insertion Variant reference sequence is populated'
                              'when it should not be in '
@@ -160,9 +161,7 @@ class VcfFileGenerator:
                 return None
             if variant['genomicVariantSequence'] == '':
                 variant['genomicVariantSequence'] = '.'
-                variant['POS'] = variant['start']
             else:
-                variant['POS'] = variant['start'] - 1
                 self._add_padded_base_to_variant(variant, 'insertion')
         elif so_term in ['point_mutation', 'MNV']:
             variant['POS'] = variant['start']
