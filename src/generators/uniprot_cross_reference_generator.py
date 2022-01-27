1f8d0bdf019a4a6f6df6509290e79805b01a04c2
markquintontulloch <markquintontulloch@gmail.com>
2022-01-27 14:23:13 +0000

    Add header to uniprot xref file

--- src/generators/uniprot_cross_reference_generator.py
+++ src/generators/uniprot_cross_reference_generator.py
@@ -3,6 +3,8 @@ import csv
 import logging
 import upload
 
+from headers import create_header
+
 logger = logging.getLogger(name=__name__)
 
 
@@ -15,10 +17,26 @@ class UniProtGenerator:
         self.config_info = config_info
         self.generated_files_folder = generated_files_folder
 
+    @classmethod
+    def _generate_header(cls, config_info, taxon_ids, data_format):
+
+        return create_header('Alliance Cross References', config_info.config['RELEASE_VERSION'],
+                             stringency_filter='Stringent',
+                             config_info=config_info,
+                             taxon_ids=taxon_ids,
+                             data_format=data_format)
+
+   
+    def _get_taxon_ids(relationships):
+        taxon_ids = set()
+        for item in cls.relationships:
+            taxon_ids.add(item['TaxonID'])
+        return taxon_ids
+    
     def _write_uniprot_file(cls, relationships, tab_file):
         logger.info('Writing output file')
         with open(cls.generated_files_folder + '/' + tab_file, 'w') as tsvfile:
-            tsvfile.write(cls.file_header + "\n")
+            tsvfile.write(cls._generate_header(cls.config_info, _get_taxon_ids(relationships), 'tsv'))
             for item in relationships:
                 tsvfile.write(item['GlobalCrossReferenceID'] + "\t" + item['GeneID'] + "\n")
 
